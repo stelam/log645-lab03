34a36df595bf8683ed87ef5a85ac67d2d5a86783
Jonathan <charestjonathan92@gmail.com>
2017-03-08 19:31:08 -0500

    petite matrice

--- lab3.cpp
+++ lab3.cpp
@@ -11,15 +11,16 @@
 #define SLEEP_TIME 5
 
 void initMPI(int *argc, char ***argv, int &number_of_processors, int &processor_rank);
-void print_matrix_at_k(double *matrix, int m, int n, int np, int k);
-void init_matrix(double *matrix, int m, int n, int np);
-int get_offset(int k, int i, int j, int m, int n);
+void print_matrix_at_k(double *matrix, int m, int n);
+void init_matrix(double *matrix, int m, int n);
+int get_offset(int i, int j, int m, int n);
 void dispatchTask(int m, int n, int nb_procs);
 void processResult(double *matrix, int m, int n, int np);
 void executeTask(int m, int n, double td, double h);
 void sequential(double *matrix, int m, int n, int np, double td, double h);
 void start_timer(double *time_start);
 double stop_timer(double *time_start);
+void copyMatrix(double *matrixSource, double *matrixDest, int n, int m);
 
 int main(int argc, char** argv) {
 	const int N = atoi(argv[1]);
@@ -29,7 +30,7 @@ int main(int argc, char** argv) {
 	const double H = atof(argv[5]);
 	const int NB_PROCS = atoi(argv[6]);
 
-	double matrix[M * N * NP];
+	double matrix[M * N];
 	int number_of_processors, processor_rank;
 	double time_seq, time_parallel, acc, time_start;
 
@@ -38,22 +39,22 @@ int main(int argc, char** argv) {
 
 	if(processor_rank == 0) {
 		printf("\n================================== Initiale ================================== \n");
-		init_matrix(matrix, M, N, NP);
+		init_matrix(matrix, M, N);
 		printf("Matrice initiale : \n");
-		print_matrix_at_k(matrix, M, N, NP, 0);
+		print_matrix_at_k(matrix, M, N);
 		printf("\n================================== Séquentiel ================================== \n");
 		start_timer(&time_start);
 		sequential(matrix, M, N, NP, TD, H);
-		printf("Matrice finale : \n");
-		print_matrix_at_k(matrix, M, N, NP, NP - 1);
 		time_seq = stop_timer(&time_start);
+		printf("Matrice finale : \n");
+		print_matrix_at_k(matrix, M, N);
 		printf("\n================================== Parallel ================================== \n");
-		init_matrix(matrix, M, N, NP);
+		init_matrix(matrix, M, N);
 		start_timer(&time_start);
 		processResult(matrix, M, N, NP);
-		printf("Matrice finale : \n");
-		print_matrix_at_k(matrix, M, N, NP, NP - 1);
 		time_parallel = stop_timer(&time_start);
+		printf("Matrice finale : \n");
+		print_matrix_at_k(matrix, M, N);
 		printf("================================================================================ \n");
 
 		acc = time_seq/time_parallel;
@@ -99,8 +100,8 @@ void initMPI(int *argc, char ***argv, int &number_of_processors, int &processor_
 *
 *   returns: the 1D index equivalent (offset)
 */
-int get_offset(int k, int i, int j, int m, int n) { 
-	return (k * m * n) + (j * m) + i; 
+int get_offset(int i, int j, int m, int n) { 
+	return (j * m) + i; 
 }
 
 
@@ -123,26 +124,29 @@ double stop_timer(double *time_start) {
 
 void sequential(double *matrix, int m, int n, int np, double td, double h) {
 
-	double ref1, ref2, ref3, ref4, ref5;
+	double ref1, ref2, ref3, ref4, ref5, matrixCurrent[m * n];
 
 	//process
 	for (int k = 1; k < np; k++) {
+
+	copyMatrix(matrix, matrixCurrent, n, m);
+
 		for (int j = 0; j < n; j++) {
 			for (int i = 0; i < m; i++) {
-				ref1 = matrix[get_offset(k-1, i, j, m, n)];
-				if(i != 0){ref2 = matrix[get_offset(k-1, i-1, j, m, n)];}
+				ref1 = matrixCurrent[get_offset(i, j, m, n)];
+				if(i != 0){ref2 = matrixCurrent[get_offset(i-1, j, m, n)];}
 				else{ref2 = 0;}
-				if(i != m-1){ref3 = matrix[get_offset(k-1, i+1, j, m, n)];}
+				if(i != m-1){ref3 = matrixCurrent[get_offset(i+1, j, m, n)];}
 				else{ref3 = 0;}
-				if(j != 0){ref4 = matrix[get_offset(k-1, i, j-1, m, n)];}
+				if(j != 0){ref4 = matrixCurrent[get_offset(i, j-1, m, n)];}
 				else{ref4 = 0;}
-				if(j != n-1){ref5 = matrix[get_offset(k-1, i, j+1, m, n)];}
+				if(j != n-1){ref5 = matrixCurrent[get_offset(i, j+1, m, n)];}
 				else{ref5 = 0;}
 
 				if (i == 0 || i == m -1 || j == 0 || j == n - 1) {
-					matrix[get_offset(k, i, j, m, n)] = 0;
+					matrix[get_offset(i, j, m, n)] = 0;
 				} else {
-					matrix[get_offset(k, i, j, m, n)] = ((1 - ((4 * td) / (h*h))) * ref1) + (td / (h*h)) * (ref2+ref3+ref4+ref5);
+					matrix[get_offset(i, j, m, n)] = ((1 - ((4 * td) / (h*h))) * ref1) + (td / (h*h)) * (ref2+ref3+ref4+ref5);
 				}
 
 				usleep(SLEEP_TIME);
@@ -169,14 +173,14 @@ void dispatchTask(int m, int n, int nb_procs) {
 					}
 					refs[0] = i;
 					refs[1] = j;
-					refs[2] = matrix[get_offset(0, i, j, m, n)];
-					if(i != 0){refs[3] = matrix[get_offset(0, i-1, j, m, n)];}
+					refs[2] = matrix[get_offset(i, j, m, n)];
+					if(i != 0){refs[3] = matrix[get_offset(i-1, j, m, n)];}
 					else{refs[3] = 0;}
-					if(i != m-1){refs[4] = matrix[get_offset(0, i+1, j, m, n)];}
+					if(i != m-1){refs[4] = matrix[get_offset(i+1, j, m, n)];}
 					else{refs[4] = 0;}
-					if(j != 0){refs[5] = matrix[get_offset(0, i, j-1, m, n)];}
+					if(j != 0){refs[5] = matrix[get_offset(i, j-1, m, n)];}
 					else{refs[5] = 0;}
-					if(j != n-1){refs[6] = matrix[get_offset(0, i, j+1, m, n)];}
+					if(j != n-1){refs[6] = matrix[get_offset(i, j+1, m, n)];}
 					else{refs[6] = 0;}
 					refs[7] = 1;
 
@@ -196,30 +200,26 @@ void dispatchTask(int m, int n, int nb_procs) {
 
 void processResult(double *matrix, int m, int n, int np) {
 	int tailleMatrix = m * n, ri, rj;
-	double currentMatrix[tailleMatrix+1], result[3], res;
+	double matrixCurrent[tailleMatrix+1], result[3], res;
 
-	for (int j = 0; j < n; j++) {
-		for (int i = 0; i < m; i++) {
-			currentMatrix[get_offset(0, i, j, m, n)] = matrix[get_offset(0, i, j, m, n)];
-		}
-	}
-	currentMatrix[tailleMatrix] = 1;
+	copyMatrix(matrix, matrixCurrent, n, m);
+	matrixCurrent[tailleMatrix] = 1;
 	
 	for (int k = 1; k < np; k++) {
-		MPI_Send(currentMatrix, tailleMatrix+1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+		MPI_Send(matrixCurrent, tailleMatrix+1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
 		for (int j = 0; j < n; j++) {
 			for (int i = 0; i < m; i++) {
 				MPI_Recv(result, 3, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 				ri = (int)(result[0]);
 				rj = (int)(result[1]);
 				res = result[2];
-				matrix[get_offset(k, ri, rj, m, n)] = res;
-				currentMatrix[get_offset(0, ri, rj, m, n)] = res;
+				matrix[get_offset(ri, rj, m, n)] = res;
+				matrixCurrent[get_offset(ri, rj, m, n)] = res;
 			}
 		}
 	}
-	currentMatrix[tailleMatrix] = 0;
-	MPI_Send(currentMatrix, tailleMatrix+1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+	matrixCurrent[tailleMatrix] = 0;
+	MPI_Send(matrixCurrent, tailleMatrix+1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
 }
 
 void executeTask(int m, int n, double td, double h) {
@@ -265,18 +265,13 @@ void executeTask(int m, int n, double td, double h) {
 *	n: size on y axis
 *
 */
-void init_matrix(double *matrix, int m, int n, int np) {
-	for (int k = 0; k < np; k++) {
-		for (int j = 0; j < n; j++) {
-			for (int i = 0; i < m; i++) {
-				if (k == 0)
-					matrix[get_offset(0, i, j, m, n)] = i * (m - i - 1) * j * (n - j - 1);
-				else {
-					matrix[get_offset(k, i, j, m, n)] = -1;
-				}
-			}
+void init_matrix(double *matrix, int m, int n) {
+	for (int j = 0; j < n; j++) {
+		for (int i = 0; i < m; i++) {
+			matrix[get_offset(i, j, m, n)] = i * (m - i - 1) * j * (n - j - 1);
 		}
 	}
+	
 }
 
 /*
@@ -287,12 +282,20 @@ void init_matrix(double *matrix, int m, int n, int np) {
 *   matrix: the 1D array containing the matrix
 *
 */
-void print_matrix_at_k(double *matrix, int m, int n, int np, int k) {
+void print_matrix_at_k(double *matrix, int m, int n) {
 	for (int j = 0; j < n; j++) {
 		for (int i = 0; i < m; i++){
-			printf("%6.1f \t", matrix[get_offset(k, i, j, m, n)]);
+			printf("%6.1f \t", matrix[get_offset(i, j, m, n)]);
 		}
 		printf("\n");
 	}
 	printf("\n\n");
+}
+
+void copyMatrix(double *matrixSource, double *matrixDest, int n, int m) {
+for (int j = 0; j < n; j++) {
+		for (int i = 0; i < m; i++) {
+			matrixDest[get_offset(i, j, m, n)] = matrixSource[get_offset(i, j, m, n)];
+		}
+	}
 }
